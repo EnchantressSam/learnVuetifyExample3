4cdffbb3ca2f18929a4cb68a5ba7759669af3709
Tatyana Loginova <tatyanka8samara@mail.ru>
2020-01-10 13:12:28 +0400

    finished example3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn text color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>mdi-email</v-icon>
+     </v-btn>
+
+     <v-btn depressed small dark color="purple">
+      <span>email me</span>
+      <v-icon right large>mdi-email</v-icon>
+     </v-btn>
+
+     <v-btn fab depressed small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+     </v-btn>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam vero deserunt dolores odit, qui necessitatibus, odio quos quas provident, exercitationem modi illo ducimus rerum id reiciendis rem at a quia!</p>
+
+     <v-btn class="hidden-md-and-down red">click me</v-btn>
+     <v-btn class="hidden-md-and-up blue">click me</v-btn>
+     <v-btn dark class="hidden-sm-only">click me</v-btn>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
